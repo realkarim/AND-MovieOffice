634b6d8ecfe96595f33d93023030212d4940cf8a
Karim <karim.mostafa.18@gmail.com>
2016-04-17 23:49:21 +0200

    Perform initial commit.

---
+++ app/src/main/java/apps/realkarim/movieoffice/MoviesActivityFragment.java
@@ -0,0 +1,22 @@
+package apps.realkarim.movieoffice;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MoviesActivityFragment extends Fragment {
+
+    public MoviesActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_movies, container, false);
+    }
+}
